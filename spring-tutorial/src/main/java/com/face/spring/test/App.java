46dd92f813f609f2fda4dde2f0d5035f5bc931c3
FacePrepJFS <12venkat21@gmail.com>
2019-11-27 15:54:02 +0530

    new spring hibernate tutorial

---
+++ spring-tutorial/src/main/java/com/face/spring/test/App.java
@@ -0,0 +1,14 @@
+package com.face.spring.test;
+
+import org.springframework.context.ApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+	ApplicationContext appcontext;
+		Person person=new Person();
+	person.display();
+
+	}
+
+}
---
+++ spring-tutorial/src/main/java/com/face/spring/test/Person.java
@@ -0,0 +1,8 @@
+package com.face.spring.test;
+
+public class Person {
+public void display() {
+	System.out.println("Hi Springs");
+
+}
+}
